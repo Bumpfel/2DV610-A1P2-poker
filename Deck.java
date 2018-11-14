c74f57394fe453c3d4ee259a23890ca269f96ddf
Eric Enoksson <ee222yb@student.lnu.se>
2018-11-14 13:39:41 +0100

    REFACTOR2 deckShouldContainCards
    removed size var

--- Deck.java
+++ Deck.java
@@ -2,18 +2,16 @@ import java.util.ArrayList;
 
 public class Deck {
 
-	private int size;
 	ArrayList<Card> cards = new ArrayList<>();
 	
 	public Deck() {
 		for(int i = 0; i < 52;  i ++) {
 			cards.add(new Card(Card.Denomination.ACE, Card.Suit.SPADES));
-			size ++;
 		}
 	}
 	
 	public int size() {
-		return size;
+		return cards.size();
 	}
 	
 	public Card getTopCard() {
