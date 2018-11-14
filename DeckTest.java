355cd1362a66b6de97c051ef46b2d5f0c949f9b1
Eric Enoksson <ee222yb@student.lnu.se>
2018-11-14 16:43:11 +0100

    RED deckShouldContain4CardsOfEachDenomination

--- DeckTest.java
+++ DeckTest.java
@@ -37,5 +37,25 @@ public class DeckTest {
 		verify(sutSpy, times(13)).createCard(any(), eq(Card.Suit.CLUBS));
 	}
 	
+	@Test
+	public void deckShouldContain4CardsOfEachDenomination() {
+		Deck sutSpy = spy(Deck.class);
+		
+		sutSpy.init();
+
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.ACE), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.TWO), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.THREE), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.FOUR), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.FIVE), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.SIX), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.SEVEN), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.EIGHT), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.NINE), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.TEN), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.JACK), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.QUEEN), any());
+		verify(sutSpy, times(4)).createCard(eq(Card.Denomination.KING), any());
+	}
 
 }
