b3ed5fc294e795f48d8bc45cd89f9b6c14cf4848
Bumpfel <ee222yb@student.lnu.se>
2018-11-15 14:43:14 +0100

    RED shouldBeAbleToGetSizeOfHand

--- PlayerTest.java
+++ PlayerTest.java
@@ -4,10 +4,21 @@ import org.junit.Test;
 
 public class PlayerTest {
 
+	Player sut;
+	
 	@Test
 	public void showHandShouldReturnIterable() {
-		Player sut = new Player();
+		sut = new Player();
 		
 		assertTrue(sut.showHand() instanceof Iterable);
+	}
+	
+	@Test
+	public void shouldBeAbleToGetSizeOfHand() {
+		sut = new Player();
+		
+		int actual = sut.getSize();
+		int expected = 0;
+		assertEquals(expected, actual);
 	}
 }
