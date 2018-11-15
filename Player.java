e1484518b03524e88fae489a23ad0b99c394fc0c
Bumpfel <ee222yb@student.lnu.se>
2018-11-15 19:08:21 +0100

    GREEN shouldDeterminePair

--- Player.java
+++ Player.java
@@ -4,6 +4,8 @@ public class Player {
 	
 	private ArrayList<Card> hand = new ArrayList<>();
 	
+	public enum Score { PAIR };
+	
 	public Iterable<Card> showHand() {
 		return hand;
 	}
@@ -20,5 +22,9 @@ public class Player {
 	
 	public void clearHand() {
 		hand.clear();
+	}
+	
+	public Score getScore() {
+		return Score.PAIR;
 	}
 }
