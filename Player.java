85e65f2448e44070ddbd4ea57d91d90a84918af5
Eric Enoksson <ee222yb@student.lnu.se>
2018-11-18 00:32:58 +0100

    GREEN shouldDetermineFourOfAKind

--- Player.java
+++ Player.java
@@ -37,6 +37,8 @@ public class Player {
 		
 		if(hand.size() != MAX_HAND_SIZE)
 			return null;
+		else if(sortedValues[12] == 4)
+			return Score.FOUR_OF_A_KIND;
 		else if(sortedValues[12] == 3 && sortedValues[11] == 2)
 			return Score.FULL_HOUSE;
 		else if(isFlush())
