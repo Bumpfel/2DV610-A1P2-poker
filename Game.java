d7de5acbd61c5c1c9f74d72634f1fbbb8c48183e
Eric Enoksson <ee222yb@student.lnu.se>
2018-11-22 22:42:28 +0100

    GREEN shouldDealFromSameDeck

--- Game.java
+++ Game.java
@@ -2,18 +2,21 @@ public class Game {
 	
 	public final int CARDS_TO_DEAL = 5;
 	private Player player;
+	private Deck deck;
 	
 	public Game(Player newPlayer) {
 		player = newPlayer;
+		deck = new Deck();
 	}
 
 	public Game(Player newPlayer, Deck newDeck) {
 		player = newPlayer;
+		deck = newDeck;
 	}
 	
 	public void newGame() {
-		Card c = new Deck().getTopCard();
 		for(int i = 0; i < CARDS_TO_DEAL; i ++) {
+			Card c = deck.getTopCard();
 			player.dealCard(c);
 		}
 	}
