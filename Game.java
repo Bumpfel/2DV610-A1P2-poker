7af8ced5d438d5e1522eda87cbf275531dffab5e
Eric Enoksson <ee222yb@student.lnu.se>
2018-11-22 22:10:42 +0100

    GREEN newGameShouldDealCardsToPlayer

--- Game.java
+++ Game.java
@@ -1,10 +1,18 @@
 public class Game {
 	
+	private Player player;
+	
 	public Game(Player newPlayer) {
+		player = newPlayer;
 	}
 	
 	public void newGame() {
-		
+		Card c = new Deck().getTopCard();
+		player.dealCard(c);
+		player.dealCard(c);
+		player.dealCard(c);
+		player.dealCard(c);
+		player.dealCard(c);
 	}
 	
 }
