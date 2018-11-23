9c848bbaf251294532d38f25c228214723ff5485
Bumpfel <ee222yb@student.lnu.se>
2018-11-23 17:32:08 +0100

    GREEN shouldStartGameIfPIsPressed

--- view/Console.java
+++ view/Console.java
@@ -7,16 +7,22 @@ public class Console {
 	public final String WELCOME_MSG = "Welcome to Poker";
 	public final String INSTRUCTIONS = "Press 'p' to play, 'q' to quit";
 
+	private Game game;
 	private ConsoleWrapper cw;
 	
-	public Console(Game game, ConsoleWrapper newCW) {
+	public Console(Game newGame, ConsoleWrapper newCW) {
+		game = newGame;
 		cw = newCW;
 	}
 	
 	public void play() {
 		printWelcomeMsg();
 		cw.println(INSTRUCTIONS);
-		cw.getInput();
+		String input = cw.getInput();
+		
+		if(input != null) {
+			game.newGame();
+		}
 	}
 	
 	public void printWelcomeMsg() {
