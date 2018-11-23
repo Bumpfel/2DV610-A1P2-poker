c81a55263680955979998e806b3f654bc8e47de1
Eric Enoksson <ee222yb@student.lnu.se>
2018-11-23 01:40:50 +0100

    RED shouldPrintWelcomeMsg

--- tests/ConsoleTest.java
+++ tests/ConsoleTest.java
@@ -1,11 +1,20 @@
 package tests;
 
+import model.*;
+import view.*;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class ConsoleTest {
 
 	@Test
-	public void shouldDoSomething() {
+	public void shouldPrintWelcomeMsg() {
+		Game mockGame = mock(Game.class);
+		Console sut = new Console(mockGame);
+		Console sutSpy = spy(sut);
 		
+		sutSpy.play();
+		
+		verify(sutSpy).printWelcomeMsg();
 	}
 }
--- view/Console.java
+++ view/Console.java
@@ -10,4 +10,8 @@ public class Console {
 	
 	public void play() {
 	}
+
+	
+	public void printWelcomeMsg() {
+	}
 }
